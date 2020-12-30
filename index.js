db4f951e5700986e2ff51d2fcbdccb87aa8931e9
CactusKing101 <chalkking17@gmail.com>
2020-12-30 14:23:13 -0500

    idk what this is

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('UwU im running');
+});
+
+client.on('message', async msg => {
+  if (!msg.content.startsWith(prefix) || msg.channel.type === 'dm' || msg.author.bot) return;
+  const args = msg.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command == 'ping') {
+    msg.channel.send('Pong');
+    msg.channel.send('*blushes :pleading_face:');
+  }
+});
